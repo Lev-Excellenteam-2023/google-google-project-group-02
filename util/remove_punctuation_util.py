830f1614800d4152c1ca838aa18951b5100743e6
bezjac <mybj20@gmail.com>
2023-08-20 23:16:41 +0300

    implemented punctuation removal from sentance

--- util/remove_punctuation_util.py
+++ util/remove_punctuation_util.py
@@ -1,13 +1,14 @@
+from string import punctuation
 from typing import List
 
 
 def remove_punctuation_from_sentence(sentence: str) -> str:
-    pass
+    return sentence.translate(str.maketrans('', '', punctuation))
 
 
-def remove_extra_white_spaces(sentence:str)->List[str]:
-    pass
+def remove_extra_white_spaces(sentence: str) -> List[str]:
+    return sentence.split()
 
 
-def prepare_sentence_for_processing(sentence:str)->List[str]:
-    pass
+def prepare_sentence_for_processing(sentence: str) -> List[str]:
+    return remove_extra_white_spaces(remove_punctuation_from_sentence(sentence))
