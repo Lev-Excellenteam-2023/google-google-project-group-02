9912bca1fa56c14aa2f13a16261dbd2f7c33a4c5
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-20 19:02:41 +0300

    Class for loadding data and tests for is functions

---
+++ data_class/file_data.py
@@ -0,0 +1,10 @@
+class file_data:
+    def __init__(self):
+        self.data_dict = {}
+        self.free_index = 0
+
+    def add_file(self, file_name: str, file_data: str):
+        return False
+
+    def get_line(self, file_index: int, line_index: int) -> str:
+        return ''
