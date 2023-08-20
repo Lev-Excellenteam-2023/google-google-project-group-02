2dc1a4907178bc6899a19949348d5170ccff5564
bezjac <mybj20@gmail.com>
2023-08-20 21:59:59 +0300

    tests for remove_punctuation_util file

---
+++ util/remove_punctuation_util.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def remove_punctuation_from_sentence(sentence: str) -> str:
+    pass
+
+
+def remove_extra_white_spaces(sentence:str)->List[str]:
+    pass
+
+
+def prepare_sentence_for_processing(sentence:str)->List[str]:
+    pass
