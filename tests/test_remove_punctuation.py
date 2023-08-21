2297b91c203732fac5f38698e65529e2ecdd49ac
bezjac <mybj20@gmail.com>
2023-08-21 10:33:18 +0300

    added documentation

--- tests/test_remove_punctuation.py
+++ tests/test_remove_punctuation.py
@@ -1,6 +1,8 @@
 import pytest
 from util import remove_punctuation_util
 
+# test file for remove_punctuation_util.py
+
 SENTENCE_01 = "H@@{%,i t#(he*re+, {have] y_o_u e~ve!!!r) s<=>ee<<n #a# w,h.a.l.e"
 SENTENCE_02 = "Hi there have you ever seen a whale"
 SENTENCE_02_WITH_SPACES = "Hi    there   have    you    ever    seen    a    whale    "
@@ -18,9 +20,9 @@ def test_remove_white_spaces():
         SENTENCE_02_WITH_SPACES)
 
 
-def test_prepare_sentence_for_process():
-    assert remove_punctuation_util.prepare_sentence_for_processing(SENTENCE_01) == ['Hi', 'there', 'have', 'you',
-                                                                                    'ever', 'seen', 'a', 'whale']
+def test_process_sentence():
+    assert remove_punctuation_util.process_sentence(SENTENCE_01) == ['Hi', 'there', 'have', 'you',
+                                                                     'ever', 'seen', 'a', 'whale']
 
 
 pytest.main(['-v'])
--- util/remove_punctuation_util.py
+++ util/remove_punctuation_util.py
@@ -2,6 +2,8 @@ from string import punctuation
 from typing import List
 
 
+# util file to process sentences (removes punctuation and extra white spaces from a string)
+
 def remove_punctuation_from_sentence(sentence: str) -> str:
     return sentence.translate(str.maketrans('', '', punctuation))
 
@@ -10,5 +12,5 @@ def remove_extra_white_spaces(sentence: str) -> List[str]:
     return sentence.split()
 
 
-def prepare_sentence_for_processing(sentence: str) -> List[str]:
+def process_sentence(sentence: str) -> List[str]:
     return remove_extra_white_spaces(remove_punctuation_from_sentence(sentence))
