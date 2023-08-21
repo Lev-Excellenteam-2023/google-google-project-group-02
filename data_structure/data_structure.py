fae1c518dd8eacfa0faf25cb2b245ccc3b64b12b
RubenYoo <rubenchiche0317@gmail.com>
2023-08-21 10:32:30 +0300

    Documentation and typing fixes

--- data_structure/data_structure.py
+++ data_structure/data_structure.py
@@ -5,23 +5,45 @@ from typing import List
 
 @dataclass
 class WordData:
+    """
+    This class save the data of a word.
+    """
+
     file: int
     row: int
     offset: int
 
     def is_my_neighbors(self, word: WordData) -> bool:
+        """
+        This function check if two words are in the same file,
+        row and the second word is the next word of the first word.
+        :param word: second word
+        :return: True if the second word is the neighbor of the first word, else False
+        """
+
         if self.file == word.file and self.row == word.row and self.offset + 1 == word.offset:
             return True
         return False
 
 
 class WordsGraph:
+    """
+    This class save all the words into a data structure.
+    """
+
     graph: dict[str, [WordData]]
 
     def __init__(self) -> None:
         self.graph = dict()
 
     def add_word(self, word: str, word_data: WordData) -> None:
+        """
+        This function add a word to the data structure.
+        :param word: the word
+        :param word_data: it's data
+        :return: None
+        """
+
         if word in self.graph:
             self.graph[word] += [word_data]
         else:
--- tests/test_data_stucture.py
+++ tests/test_data_stucture.py
@@ -1,24 +1,28 @@
 from data_structure.data_structure import WordData, WordsGraph
 
 
-def test_is_my_neighbors():
-    my_word_data = WordData(0, 0, 0)
-    my_second_word_data = WordData(0, 0, 1)
+def test_is_my_neighbors() -> None:
+    my_word_data: WordData = WordData(0, 0, 0)
+    my_second_word_data: WordData = WordData(0, 0, 1)
 
     assert my_word_data.is_my_neighbors(my_second_word_data) is True
+    assert my_second_word_data.is_my_neighbors(my_word_data) is False
 
 
 def test_add_word():
-    my_graph = WordsGraph()
-    my_word_data = WordData(0, 0, 0)
+    my_struct: WordsGraph = WordsGraph()
+    my_word_data: WordData = WordData(0, 0, 0)
+    my_second_word_data: WordData = WordData(0, 0, 1)
 
-    my_graph.add_word("hello", my_word_data)
+    my_struct.add_word("hello", my_word_data)
 
-    assert my_graph.graph["hello"] == [my_word_data]
+    assert my_struct.graph["hello"] == [my_word_data]
+    assert my_struct.graph["hello"] != [my_second_word_data]
 
 
 def test_find_match():
-    my_struct = WordsGraph()
+    my_struct: WordsGraph = WordsGraph()
+
     my_struct.add_word("hello", WordData(0, 0, 0))
     my_struct.add_word("world", WordData(0, 0, 1))
     my_struct.add_word("this", WordData(0, 0, 2))
