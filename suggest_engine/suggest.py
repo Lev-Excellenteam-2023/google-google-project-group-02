5c6b378afe7b2bec72efb03f0860dc05a6448a7b
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-22 11:14:35 +0300

    score_check

--- suggest_engine/suggest.py
+++ suggest_engine/suggest.py
@@ -2,6 +2,31 @@ from typing import List, Tuple
 from data_class.file_data import FileData
 
 
+
+def help_get_score(user_input: str, sentence_substring: str, score: int, score_to_sub: int) -> int:
+    if user_input[:5] == sentence_substring[:5]:
+        return score - score_to_sub
+    for iterator in range(5):
+        if user_input[iterator] != sentence_substring[iterator]:
+            score -= (5 - iterator) * score_to_sub
+            break
+    return score
+
+
+def get_score(user_input: str, sentence_substring: str) -> int:
+    score = len(user_input) * 2
+    if user_input == sentence_substring:
+        return score
+    if len(user_input) == len(sentence_substring):
+        score -= 2
+        return help_get_score(user_input, sentence_substring, score, 1)
+    elif len(user_input) > len(sentence_substring):
+        score -= 2
+        return help_get_score(user_input, sentence_substring, score, 2)
+    else:
+        return help_get_score(user_input, sentence_substring, score, 2)
+
+
 def find_match(user_input: List[str], data: FileData) -> List[Tuple]:
     """
     This function the best 5 sentence that match to the given string.
@@ -18,6 +43,7 @@ def find_match(user_input: List[str], data: FileData) -> List[Tuple]:
         if str_input in row_content:
             suggestions += [(row_content, index_word.file, index_word.row)]
     return list(set(suggestions))
+
 
 
 def cut_sentence_till_first_word(sentence: str, index: int) -> str:
@@ -57,6 +83,11 @@ def sub_char(user_input: str, index: int, sentence: str, first_word_index: int) 
     sentence = cut_sentence_till_first_word(sentence, first_word_index)
     return sentence.find(pre_str) == 0 and sentence.find(suf_str) == index
 
+def find_replace_one_char(user_input: str, sentence: str, first_word_index: int):
+    for index in range(len(user_input)):
+        if replaced_char(user_input, index, sentence, first_word_index):
+            pass
+
 
 def find_suggestions_with_a_mistake(user_input: str, data: FileData, first_word: str, num_of_found_suggestions: int) \
         -> List[Tuple]:
@@ -70,7 +101,21 @@ def find_suggestions_with_a_mistake(user_input: str, data: FileData, first_word:
                 if replaced_char(user_input, iterator, data.get_line(file_index, row_index), word_offset):
                     suggests.append((file_index, row_index))
 
-    return suggests
+    # for
+    # suggests = list()
+    # if len(first_word) < 5:
+    #     for index_word in data.words_graph.graph[first_word]:
+    #         file_index = index_word.file
+    #         row_index = index_word.row
+    #         word_offset = index_word.offset
+    #         for iterator in range(5, len(user_input)):
+    #             if replaced_char(user_input, iterator, data.get_line(file_index, row_index), word_offset):
+    #                 suggests.append((file_index, row_index))
+    # suggests = add_row_content(suggests, data)
+    # if len(suggests) > 5:
+    #     return suggests
+
+    # return suggests
 
 
 def match_first_word_mistaken(user_input: List[str], data: FileData, first_word: str, num_of_found_suggestions: int = 0) \
@@ -89,6 +134,7 @@ def find_top_five_completions(user_input: List[str], data: FileData) -> List[Tup
     if first_word in data.words_graph.graph:
         suggestions = find_match(user_input, data)
         return sort_and_filter_first_k(suggestions, 5)
+
     #     if len(suggestions) < 5:
     #         new_suggestions = find_suggestions_with_a_mistake(' '.join(user_input), data, first_word, len(suggestions))
     #         if len(suggestions) + len(new_suggestions) > 5:
@@ -101,5 +147,4 @@ def find_top_five_completions(user_input: List[str], data: FileData) -> List[Tup
     #         suggestions += new_suggestions
     # else:
     #     suggestions = match_first_word_mistaken(user_input, data)
-
-
+    # return suggestions
--- tests/test_suggest.py
+++ tests/test_suggest.py
@@ -1,6 +1,6 @@
 from data_class.file_data import FileData
 from suggest_engine.suggest import find_match, replaced_char, add_char, sub_char, \
-    sort_and_filter_first_k
+    sort_and_filter_first_k, get_score
 
 data = FileData('..\\private\\my_files')
 
@@ -37,3 +37,24 @@ def test_sort_and_filter_first_k():
     try_list = [('hi', 1, 5), ('go', 2, 17), ('python', 3, 5), ('went', 7, 5), ('apple', 10, 5), ('git', 15, 5)]
     res_list = [('apple', 10, 5), ('git', 15, 5), ('go', 2, 17), ('hi', 1, 5), ('python', 3, 5)]
     assert sort_and_filter_first_k(try_list, 5) == res_list
+
+
+def test_get_score():
+    assert get_score('my code', 'my code') == 14
+    assert get_score('my core', 'my code') == 11
+    assert get_score('my lode', 'my code') == 10
+    assert get_score('mylcore', 'my code') == 9
+    assert get_score('mi core', 'my code') == 8
+    assert get_score('ny core', 'my code') == 7
+    assert get_score('my codre', 'my code') == 12
+    assert get_score('my pcode', 'my code') == 10
+    assert get_score('myp code', 'my code') == 8
+    assert get_score('mpy code', 'my code') == 6
+    assert get_score('lmy code', 'my code') == 4
+    assert get_score('my coe', 'my code') == 10
+    assert get_score('my ode', 'my code') == 8
+    assert get_score('mycode', 'my code') == 6
+    assert get_score('m code', 'my code') == 4
+    assert get_score('y code', 'my code') == 2
+
+
